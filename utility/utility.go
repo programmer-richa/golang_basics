68e9fc6d11dae54e6a9957da9be10eb8493f368e
programmer-richa <programmer.richa@gmail.com>
2020-04-09 13:29:40 +0530

    Common functions used in module are defined here

--- utility/utility.go
+++ utility/utility.go
@@ -5,22 +5,23 @@ import (
 	"strings"
 )
 
-//Tabs level wise spaces
+//Tabs   This function generates '\t' strings as per user requirement. It accepts argument: level:Specifies Number of tab keys to ge generated
 func Tabs(level int) string {
 	if level < 1 {
 		level = 0
 	}
+	// call built-in string function Repeat to repeat the string
 	tabSpaces := strings.Repeat("\t", level)
 	return tabSpaces
 }
 
-//Printh  print tabs and arguments passed
+//Printh   This function prints heading in a standardized format. It accepts arguments : level:Specifies Number of tab keys to ge generated and a: Holds Variadic arguments to be printed
 func Printh(level int, a ...interface{}) {
 	tabSpaces := Tabs(level)
 	arguments := tabSpaces + "\n" + tabSpaces
 
 	for _, value := range a {
-		// concatinating arguments to string
+		// concatinating arguments to string after getting the value from the interface
 		arguments += fmt.Sprintf("%v ", value)
 	}
 	arguments += tabSpaces + "\n" + tabSpaces + "__________________________________________________________________________"
@@ -28,25 +29,25 @@ func Printh(level int, a ...interface{}) {
 
 }
 
-//Println  print tabs and arguments passed
+//Println   This function prints data in a standardized format and generates a newline after printing. It accepts arguments : level:Specifies Number of tab keys to ge generated and a: Holds Variadic arguments to be printed
 func Println(level int, a ...interface{}) {
 
 	arguments := Tabs(level)
 
 	for _, value := range a {
-		// concatinating arguments to string
+		// concatinating arguments to string after getting the value from the interface
 		arguments += fmt.Sprintf("%v ", value)
 	}
 	fmt.Println(arguments)
 }
 
-//Print  print tabs and arguments passed
+//Print   This function prints data in a standardized format without generating a newline after command execution. It accepts arguments : level:Specifies Number of tab keys to ge generated and a: Holds Variadic arguments to be printed
 func Print(level int, a ...interface{}) {
 
 	arguments := Tabs(level)
 
 	for _, value := range a {
-		// concatinating arguments to string
+		// concatinating arguments to string after getting the value from the interface
 		arguments += fmt.Sprintf("%v ", value)
 	}
 	fmt.Print(arguments)
