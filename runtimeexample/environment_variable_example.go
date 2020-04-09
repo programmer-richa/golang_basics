2c580085113bbdf893025bc59660314b1559e439
programmer-richa <programmer.richa@gmail.com>
2020-04-09 13:27:24 +0530

    Frtching runtime environment details

--- runtimeexample/environment_variable_example.go
+++ runtimeexample/environment_variable_example.go
@@ -1,17 +1,19 @@
 package runtimeexample
 
 import (
+	"runtime"
+
 	"github.com/programmer-richa/golang_basics/utility"
 )
 
 // Module Level Flag -- used for indentation Purpose
 const level = 1
 
-//GettingRuntimeEnvData The GOARCH, GOOS, GOPATH, and GOROOT environment variables complete the set of Go environment variables. They influence the building of Go programs (see https://golang.org/cmd/go and https://golang.org/pkg/go/build). GOARCH, GOOS, and GOROOT are recorded at compile time and made available by constants or functions in this package, but they do not influence the execution of the run-time system.
-func GettingRuntimeEnvData() {
+//Data The GOARCH, GOOS, GOPATH, and GOROOT environment variables complete the set of Go environment variables. They influence the building of Go programs (see https://golang.org/cmd/go and https://golang.org/pkg/go/build). GOARCH, GOOS, and GOROOT are recorded at compile time and made available by constants or functions in this package, but they do not influence the execution of the run-time system.
+func Data() {
 	utility.Printh(level, "Using runtime Package")
-	// utility.Println(level, "GOARCH: ", runtime.GOARCH)
-	// utility.Println(level, "GOOS: ", runtime.GOOS)
+	utility.Println(level, "GOARCH: ", runtime.GOARCH)
+	utility.Println(level, "GOOS: ", runtime.GOOS)
 	// utility.Println(level,"GOPATH: ", runtime.GOPATH)
 	// utility.Println(level,"GOROOT : ", runtime.GOROOT)
 }
