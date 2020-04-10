36afb71514fd4a056864b4f38a88060040658a2e
programmer-richa <programmer.richa@gmail.com>
2020-04-10 11:34:17 +0530

    Fetching runtime environmental variables

--- runtimeexample/environment_variable_example.go
+++ runtimeexample/environment_variable_example.go
@@ -1,3 +1,4 @@
+// Package runtimeexample demonstrate how environment variables can be fetched in go lang.
 package runtimeexample
 
 import (
@@ -6,10 +7,13 @@ import (
 	"github.com/programmer-richa/golang_basics/utility"
 )
 
-// Module Level Flag -- used for indentation Purpose
+// Module Level Flag is used for indentation purpose.
 const level = 1
 
-//Data The GOARCH, GOOS, GOPATH, and GOROOT environment variables complete the set of Go environment variables. They influence the building of Go programs (see https://golang.org/cmd/go and https://golang.org/pkg/go/build). GOARCH, GOOS, and GOROOT are recorded at compile time and made available by constants or functions in this package, but they do not influence the execution of the run-time system.
+//Data demonstrated usage of GOARCH, GOOS, GOPATH, and GOROOT environment variables.
+//  They influence the building of Go programs (see https://golang.org/cmd/go and https://golang.org/pkg/go/build).
+//  GOARCH, GOOS, and GOROOT are recorded at compile time and made available by constants or functions in this package,
+//  but they do not influence the execution of the run-time system.
 func Data() {
 	utility.Printh(level, "Using runtime Package")
 	utility.Println(level, "GOARCH: ", runtime.GOARCH)
